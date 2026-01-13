98c08efb0b08196ce46167e9eebfc95aa6764446
ms <jjum5panda@gmail.com>
2026-01-13 21:17:02 +0900

    first commit

---
+++ src/ch02/sec01/VariableExchangeExample.java
@@ -0,0 +1,15 @@
+package ch02.sec01;
+
+public class VariableExchangeExample {
+    public static void main(String[] args){
+        int x = 3;
+        int y = 5;
+
+        int temp = y;
+        y = x;
+        x = temp;
+
+        System.out.println("x: " + x + ", y: " +y);
+        System.out.printf("x: d%, y: %d\n", x, y); // d:정수, f:실수, s:문자열, b:불린, c:문자
+    }
+}
