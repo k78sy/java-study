3f73aca3b59bd941a892a4215545c218281a62a9
ms <jjum5panda@gmail.com>
2026-01-19 11:48:12 +0900

    class method

--- src/ch06/sec08/PracticeMethod.java
+++ src/ch06/sec08/PracticeMethod.java
@@ -10,4 +10,81 @@ public class PracticeMethod {
     public int random(int min, int max){
         return (int)((Math.random() * (max-min)) + min);
     }
+
+    // 2023.1.19
+    public String getGrade(int score){
+        if(score > 90){
+            return "A";
+        }else if(score > 80){
+            return "B";
+        }else{
+            return "C";
+        }
+    }
+
+    public void printStar(int num){
+        System.out.println();
+//        for(int i=0; i<num; i++){
+//            System.out.print("*");
+//        }
+        String star = "*";
+        for(int i=0; i<num; i++){
+            System.out.print(star);
+        }
+    }
+
+//    public void printStarTraiangle(int str){
+//        for(int i=0;i<str; i++){
+//            for(int k=0; k<=i; k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+//    }
+    public void printStarTraiangle(int star){
+        for(int i=0;i<star; i++){
+            printStar(i);
+        }
+        System.out.println();
+    }
+    public void printStarRectangle(int star){
+        for(int i=0; i<star; i++){
+            printStar(star);
+        }
+        System.out.println();
+    }
+    public void printStarP(int star){
+        for(int i=0; i<star; i++){
+            printStar(i);
+        }
+        for(int i=star; i>0; i--){
+            printStar(i);
+        }
+        System.out.println();
+    }
+    public void gugudan(int num){
+        for(int i=1; i<=9; i++){
+            int result = num * i;
+            System.out.printf("%d X %d = %02d\n", num, i, result);
+        }
+//        System.out.println();
+//        for(int i=1; i<=9; i++){
+//            int result = num * i;
+//            System.out.println(num + "X" + i + "=" + result);
+//        }
+    }
+    public void gugudan(int start, int end){
+        for(int k=start; k<=end; k++) {
+            System.out.printf("----%d단----\n",k);
+            gugudan(k);
+        }
+    }
+    public int sum(int startNum, int endNum){
+        int sum = 0;
+        for(int i=startNum; i<=endNum; i++){
+            sum += i;
+        }
+        return sum;
+    }
 }
--- src/ch06/sec08/PracticeMethodMain.java
+++ src/ch06/sec08/PracticeMethodMain.java
@@ -16,5 +16,49 @@ public class PracticeMethodMain {
         int r3 = pm.random(2,10); //2~9 랜덤값 리턴
         int r4 = pm.random(10,15); //10~14 랜덤값 리턴
         System.out.println("r3: " + r3 + ", r4: " + r4);
+
+        // 2026.1.19
+        /* 90점 초과는 콘솔에 A, 80점 초과는 B, 나머지는 C 리턴*/
+        String grade = pm.getGrade(100);
+        System.out.println("grade: " + grade);
+
+        String grade2 = pm.getGrade(60);
+        System.out.println("grade2: " + grade2);
+
+        int score = (int)((Math.random() * 81.0) + 20);
+        String grade3 = pm.getGrade(score);
+        System.out.printf("%d점 %s", score, grade3);
+
+        System.out.println("-----------------");
+        pm.printStar(5); //*****
+        pm.printStar(3); //***
+
+        pm.printStar(pm.random(1,6));
+        System.out.println("-----------------");
+        pm.printStarTraiangle(4);
+        //*
+        //**
+        //***
+        //****
+        System.out.println("-----------------");
+        pm.printStarRectangle(4);
+        //****
+        //****
+        //****
+        //****
+        System.out.println("-----------------");
+        pm.printStarP(pm.random(3,6));
+        System.out.println("-----------------");
+        pm.gugudan(5);
+        // 5 X 1 =5;
+        // 5 X 2 =10;
+        // .
+        // 5 X 9 =45;
+        System.out.println("-----------------");
+        pm.gugudan(3,6);
+
+        int sum = pm.sum(10,50); //10~50까지 순차적으로 더한 값을 리턴
+        System.out.println("합계: " + sum);
+        // 1 2 3 4 5
     }
 }
